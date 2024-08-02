3af3417b8c8a5f7e69c73d96cc46662400d171c0
Ulric <ulricaird1@gmail.com>
2024-08-02 19:13:13 -0400

    ai chat bot

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,12 +1,31 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import {GoogleGenerativeAI} from "@google/generative-ai"
+
+const ai = new GoogleGenerativeAI(process.env.GEMINI_KEY)
+const model = ai.getGenerativeModel({
+    model: "gemini-pro",
+})
+
+export const metadata = {
+    title: "Chat with AI",
+    description: "Chat with AI"
+}
 
 export async function POST(request) {
     const { message } = await request.json();
-
     const aiResponse = `${message}`;
 
+    const chat = model.startChat({
+        generationConfig: {
+            maxOutputTokens: 100,
+        },
+    })
+
+    const result = await chat.sendMessage(message);
+    const response = result.response.text();
+
     return NextResponse.json({
-        response: aiResponse,
+        response: response,
     }, {
         status: 200,
         headers: { 'Content-Type': 'application/json' }
