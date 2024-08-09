e44dae9bdf8a97aaa446d8535164ee9dc6aca520
Ulric <ulricaird1@gmail.com>
2024-08-09 19:50:08 -0400

    update

--- src/app/directory/Searchbar.jsx
+++ src/app/directory/Searchbar.jsx
@@ -7,17 +7,7 @@ const SearchBar = ({ onSearch }) => {
         type="text"
         placeholder="Search files..."
         onChange={(e) => onSearch(e.target.value)}
-        className="
-                   bg-white 
-                   text-gray-900
-                   placeholder-gray-1d00
-                   font-semibold
-                   border border-gray-300
-                   rounded-md shadow-sm 
-                   focus:outline-none focus:ring-2 
-                   focus:ring-blue-500 
-                   focus:border-blue-500
-                   "
+        className="bg-white text-gray-900 placeholder-gray-1d00 font-semibold border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
       />
       
     </div>
