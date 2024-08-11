919b25a8cc9736d0aee8976aeaf6f356c803d984
Ulric <ulricaird1@gmail.com>
2024-08-11 16:02:43 -0400

    new update

--- src/components/navlinks.js
+++ src/components/navlinks.js
@@ -2,7 +2,7 @@ module.exports = [
     { text: "Home", link: "/", image: "/images/logo.png" },
     { text: "About", link: "/about", image: "/nav/about.png" },
     { text: "Blog", link: "/blog", image: "/nav/blog.png" },
-    {text: "Open Source", link: "/directory"},
+    {text: "Open Source", link: "/directory", image: "/nav/open_source.png"},
     { 
       text: "Products",
       link: "/",
