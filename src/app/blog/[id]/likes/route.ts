79e28704d82b2852d056ab75d17acd58948584e4
Ulric <ulricaird1@gmail.com>
2024-08-13 12:12:43 -0400

    Blog Post Like Update

--- src/app/blog/[id]/likes/route.ts
+++ src/app/blog/[id]/likes/route.ts
@@ -5,13 +5,23 @@ export async function GET(request: NextRequest, {params}: {params: {id: string}}
     const id = Number(params.id);
     
     const supa_db = SupabaseClient().from("Blog Likes")
-    const {data, error} = await supa_db.select().eq("blog_id", id)
+    const {error, data} = await supa_db.select().eq("blog_id", id)
 
     if (error) {
         return new NextResponse("Error Fetching Likes", {status: 200});
     }
 
-    let blog_likes_data = data[0];
+    let blog_likes_data = data.length !== 0 ? data[0] : null;
+
+    if (blog_likes_data === null) {
+        let temp_update_data = await supa_db.upsert({
+            blog_id: id,
+            likes: 0,
+        }).select("*")
+
+        blog_likes_data = temp_update_data.data?.map(data => data.blog_id === id)[0]
+    }
+
     return NextResponse.json(blog_likes_data, {status: 200})
 }
 
