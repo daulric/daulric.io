4d4ed021a0c33c748225294c1f109aa544d52a93
Ulric <ulricaird1@gmail.com>
2024-09-02 11:13:08 -0400

    add links to it

--- src/app/beta/links.js
+++ src/app/beta/links.js
@@ -2,4 +2,5 @@ module.exports = [
     {title: "Youtube Downloader", description: "Convert Youtube Videos to MP3 or MP4", icon: "⏬", url: "beta/downloader/youtube"},
     {title: "Notification", description: "Send Notification to People who enabled the Notification Permission", icon: "🔔", url: "beta/notification"},
     {title: "Domain Lookup", description: "Lookup a information about a domain", icon: "🔍", url: "beta/whois"},
+    {title: "Income Handler", description: "Manage Your Income using this tool", icon: "💵", url: "beta/income"}
 ]
