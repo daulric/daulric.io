6b9ecf447ccd8fd59d25e19bed2a097f7c30d45a
Ulric <ulricaird1@gmail.com>
2024-09-02 05:13:57 -0400

    links update for beta page

--- src/app/beta/links.js
+++ src/app/beta/links.js
@@ -2,5 +2,4 @@ module.exports = [
     {title: "Youtube Downloader", description: "Convert Youtube Videos to MP3 or MP4", icon: "⏬", url: "beta/downloader/youtube"},
     {title: "Notification", description: "Send Notification to People who enabled the Notification Permission", icon: "🔔", url: "beta/notification"},
     {title: "Domain Lookup", description: "Lookup a information about a domain", icon: "🔍", url: "beta/whois"},
-    {title: "Ai Image Generator", description: "Generate images for no reason using AI", icon: "📷", url: "beta/ai/image"}
 ]
